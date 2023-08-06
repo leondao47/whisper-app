a2434be58530759a8004142cad66ad871f8c740a
leondao47 <luyendd47@gmail.com>
2023-08-06 19:50:20 +0700

    update README

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# whisper-app
+## Installation
+
+Requirements:
+
+- `python3` (version higher or equal to 3.7, at least 3.9 is recommended)
+- `ffmpeg` (see instructions for installation on the [whisper repository](https://github.com/openai/whisper)
+
+### Install `ffmpeg`:
+
+```bash
+# on Ubuntu or Debian
+sudo apt update && sudo apt install ffmpeg
+
+# on Arch Linux
+sudo pacman -S ffmpeg
+
+# on MacOS using Homebrew (https://brew.sh/)
+brew install ffmpeg
+
+# on Windows using Chocolatey (https://chocolatey.org/)
+choco install ffmpeg
+
+# on Windows using Scoop (https://scoop.sh/)
+scoop install ffmpeg
+```
+
+### Installation steps:
+
+```bash
+# clone the project
+git clone https://github.com/leondao47/whisper-app.git
+
+# create and active virtual environment
+cd whisper-app
+python3 -m venv venv
+source venv/bin/activate
+
+# install dependencies
+pip3 install git+https://github.com/linto-ai/whisper-timestamped
+pip install flask flask_cors
+
+# run application
+cd be
+python app.py
+
+```
+
+Now App is running on http://127.0.0.1:5000
--- be/app.py
+++ be/app.py
@@ -2,7 +2,7 @@ import time
 import uuid
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import whisper_timestamped as whisper
 
@@ -12,10 +12,13 @@ model = whisper.load_model("tiny.en", device="cpu")
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def serve_index():
+    return send_file("../fe/index.html")
 
-# Endpoint to receive audio file via POST request
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    start = time.time()
     if 'audio' not in request.files:
         return jsonify({'message': 'No audio part in the request'}), 400
 
@@ -26,13 +29,13 @@ def upload_file():
 
     # Save the audio file to the desired location (change 'uploads' to your preferred directory)
     save_file_name = f'{audio_file.filename}_{uuid.uuid4()}'
-    audio_file.save('uploads/' + save_file_name)
-    audio = whisper.load_audio('uploads/' + save_file_name)
+    audio_file.save(save_file_name)
+    audio = whisper.load_audio(save_file_name)
     result = whisper.transcribe(model, audio)
-    os.remove(f'uploads/{save_file_name}')
+    os.remove(f'{save_file_name}')
 
+    print(f'time: {time.time() - start}')
     return jsonify({'message': 'Audio uploaded successfully!', 'result': result}), 200
-
 
 if __name__ == '__main__':
     app.run(debug=True)
